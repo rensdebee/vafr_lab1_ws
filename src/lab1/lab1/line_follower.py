63647203aa116d69e67b064d62fa76066c12983b
Rensdebee <r.35rens@gmail.com>
2024-11-04 15:48:17 +0100

    Update code to find center of line

--- src/lab1/lab1/line_follower.py
+++ src/lab1/lab1/line_follower.py
@@ -9,21 +9,26 @@ import cv2
 from cv_bridge import CvBridge
 from lab1.utils import UTILS
 
+
 class LineFollower(Node):
     def __init__(self):
         # Init node
-        super().__init__('line_follower')
+        super().__init__("line_follower")
 
         # Init utils node (LED + LCD)
         self.utils = UTILS(self)
-        
+
         # Init recieving image from rae
         self.br = CvBridge()
-        self.image_subscriber = self.create_subscription(Image, "/rae/right/image_raw", self.image_callback, 10)
+        self.image_subscriber = self.create_subscription(
+            Image, "/rae/right/image_raw", self.image_callback, 10
+        )
         self.image_subscriber
         self.image = None
 
-        self.timer = self.create_timer(timer_period_sec=0.06, callback=self.timer_callback)
+        self.timer = self.create_timer(
+            timer_period_sec=0.06, callback=self.timer_callback
+        )
         self.should_move = False
         self.finalization_countdown = None
 
@@ -37,90 +42,158 @@ class LineFollower(Node):
         (Height_upper_boundary, Height_lower_boundary,
         Width_left_boundary, Width_right_boundary)
         """
-        ## Update these values to your liking.
+        return (0, height, 0, width)
 
-        return (0, height//2, width//4, 3*width//4)
-    
-    def get_contour_data(self, mask, out):
-        linesP = cv2.HoughLinesP(mask, rho=1, theta=np.pi / 180, threshold=250, minLineLength=90, maxLineGap=10)
-    
+    def get_lines(self, mask, out):
+        """
+        Detect lines using hough transform
+        Mask: Binary mask to detect lines on
+        Out: Original images to draw line on
+
+        Return list of lines, each line is represented by a 4-element vector (x1, y1, x2, y2)
+        """
+        # CV2 Hough transform
+        linesP = cv2.HoughLinesP(
+            mask,
+            rho=1,
+            theta=np.pi / 180,
+            threshold=50,
+            minLineLength=90,
+            maxLineGap=5,
+        )
+
+        # Draw lines on original image
         if linesP is not None:
             for i in range(0, len(linesP)):
                 l = linesP[i][0]
-                cv2.line(out, (l[0], l[1]), (l[2], l[3]), (0,0,255), 4)
+                cv2.line(out, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 4)
         else:
             linesP = []
 
-
         return linesP
-    
 
     def image_callback(self, data):
         # Convert ROS Image message to OpenCV image
         current_frame = self.br.imgmsg_to_cv2(data)
+        # TODO Image corretion code here
         self.image = current_frame
-    
+
     def edge_detector(self, current_frame):
+        # TODO Maybe import this from edge_detector.py with a command line argument to do different methods
+
         # Convert the image to grayscale
         current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
 
         # Apply a threshold to binarize the image (only keep bright areas)
-        _, current_frame = cv2.threshold(current_frame, 150, 255, cv2.THRESH_BINARY)
+        _, binary = cv2.threshold(current_frame, 250, 255, cv2.THRESH_BINARY)
 
-        # Setting parameter values 
-        t_lower = 50  # Lower Threshold 
-        t_upper = 150  # Upper threshold 
-        
-        # Applying the Canny Edge filter 
-        current_frame = cv2.Canny(current_frame, t_lower, t_upper) 
-        kernel = np.ones((5, 5), np.uint8)
-        edges_dilated = cv2.dilate(current_frame, kernel)
-        return edges_dilated
-    
+        # Setting parameter values
+        t_lower = 50  # Lower Threshold
+        t_upper = 150  # Upper threshold
+
+        # Applying the Canny Edge filter
+        canny = cv2.Canny(binary, t_lower, t_upper)
+
+        # Dilate the edges to make it easier for Hough transform
+        kernel = np.ones((7, 7), np.uint8)
+        edges_dilated = cv2.dilate(canny, kernel)
+        return edges_dilated, binary
+
     def timer_callback(self):
         # Wait for the first image to be received
         if type(self.image) != np.ndarray:
             return
-        
-        image = self.image.copy()
-        self.height = image.shape[0] 
-        self.width = image.shape[1]
 
-        crop_h_start, crop_h_stop, crop_w_start, crop_w_stop = self.crop_size(self.height, self.width)
-        mask = self.edge_detector(image)
-        # get the bottom part of the image (matrix slicing)
+        # Get image stats
+        image = self.image.copy()
+        self.height = image.shape[0]
+        self.width = image.shape[1]
+        # Edge detection
+        mask, binary = self.edge_detector(image)
+        image = cv2.cvtColor(binary, cv2.COLOR_GRAY2BGR)
+        # Crop only top part to detect lines from
+        crop_h_start, crop_h_stop, crop_w_start, crop_w_stop = self.crop_size(
+            self.height, self.width
+        )
         crop = mask[crop_h_start:crop_h_stop, crop_w_start:crop_w_stop]
 
+        # Get lines using hough and draw on image crop
+        lines = self.get_lines(
+            crop, image[crop_h_start:crop_h_stop, crop_w_start:crop_w_stop]
+        )
 
-        # output = image
-        lines = self.get_contour_data(crop, image[crop_h_start:crop_h_stop, crop_w_start:crop_w_stop])
+        # Get closest line
+        error = np.inf
+        min_i = None
+        for i in range(0, len(lines)):
+            line = lines[i][0]
+            line_center_w = crop_w_start + ((line[0] + line[2]) / 2)
+            line_error = line_center_w - (self.width / 2)
+
+            if line_error < error:
+                error = line_error
+                min_i = i
+        if min_i:
+            line = lines[min_i][0]
+            cv2.circle(
+                image,
+                (int(crop_w_start + ((line[0] + line[2]) / 2)), 0),
+                5,
+                (0, 255, 0),
+                7,
+            )
+            # Draw middle point of closest line
+            cv2.circle(
+                image,
+                (
+                    int(crop_w_start + (line[0] + line[2]) / 2),
+                    int(crop_h_start + (line[1] + line[3]) / 2),
+                ),
+                5,
+                (0, 255, 0),
+                7,
+            )
+
+        # Draw box which has been cropped
+        cv2.rectangle(
+            image,
+            (crop_w_start, crop_h_start),
+            (crop_w_stop, crop_h_stop),
+            (255, 0, 0),
+            2,
+        )
+
+        # Draw number of lines detected to RAE screen
         self.utils.draw_text(f"{len(lines)} lines")
+
         # Show the output image to the user
         cv2.imshow("output", image)
         # Print the image for 5milis, then resume execution
         cv2.waitKey(5)
-        
+
+        # TODO Turn robot towards center of closest line and drive forward
+        # TODO Define stop condition
 
 
 def main(args=None):
-  
-  # Initialize the rclpy library
-  rclpy.init(args=args)
 
-  # Create the node
-  line_follower = LineFollower()
-  
-  # Spin the node so the callback function is called.
-  rclpy.spin(line_follower)
-  
-  # Destroy the node explicitly
-  # (optional - otherwise it will be done automatically
-  # when the garbage collector destroys the node object)
-  line_follower.destroy_node()
-  
-  # Shutdown the ROS client library for Python
-  rclpy.shutdown()
-  
-if __name__ == '__main__':
-  main()
+    # Initialize the rclpy library
+    rclpy.init(args=args)
 
+    # Create the node
+    line_follower = LineFollower()
+
+    # Spin the node so the callback function is called.
+    rclpy.spin(line_follower)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    line_follower.destroy_node()
+
+    # Shutdown the ROS client library for Python
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
--- src/lab1/lab1/utils.py
+++ src/lab1/lab1/utils.py
@@ -8,16 +8,15 @@ import numpy as np
 import os
 import random
 
+
 class UTILS:
     def __init__(self, node):
         self.node = node
-        self.publisher = self.node.create_publisher(
-                    LEDControl, "/leds", 40)
-        self.publisher_image = self.node.create_publisher(Image, 'lcd', 10)
-        self.publisher_led = self.node.create_publisher(LEDControl, 'leds', 10)
+        self.publisher = self.node.create_publisher(LEDControl, "/leds", 40)
+        self.publisher_image = self.node.create_publisher(Image, "lcd", 10)
+        self.publisher_led = self.node.create_publisher(LEDControl, "leds", 10)
 
         self.bridge = CvBridge()
-
 
     def random_hex_color(self):
         return "#{:06x}".format(random.randint(0, 0xFFFFFF))
@@ -25,12 +24,12 @@ class UTILS:
     def draw_text(self, text, size=25):
         # Define image size, bar, and padding properties
         width, height = 160, 80
-    
+
         # Create new image with white background
-        img = PILImage.new('RGB', (width, height), "black")
+        img = PILImage.new("RGB", (width, height), "black")
         d = ImageDraw.Draw(img)
 
-        font_path = os.path.join(cv2.__path__[0],'qt','fonts','DejaVuSans.ttf')
+        font_path = os.path.join(cv2.__path__[0], "qt", "fonts", "DejaVuSans.ttf")
         font = ImageFont.truetype(font_path, size=size)
         text_width, text_height = d.textsize(text, font=font)
         text_x = (width - text_width) // 2
@@ -42,24 +41,24 @@ class UTILS:
         img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
         # Convert OpenCV image to ROS image and publish
         img_msg = self.bridge.cv2_to_imgmsg(img_cv, encoding="bgr8")
-        self.publisher_image.publish(img_msg) 
+        self.publisher_image.publish(img_msg)
 
     def _publish(self, msg):
         self.publisher.publish(msg)
 
-
     def _hex_to_rgb(self, hex):
         """Convert a hex color to an RGB tuple."""
-        value = hex.lstrip('#')
+        value = hex.lstrip("#")
         lv = len(value)
-        return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+        return tuple(int(value[i : i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
     def _normalize(self, num):
         """Normalize a number to a float between 0 and 1."""
         if num == 0:
             return float(num)
         else:
-            return float(num)/255.0
+            return float(num) / 255.0
+
     def _color(self, hex, brightness, freq):
         r, g, b = self._hex_to_rgb(hex)
         color_msg = ColorPeriod()
@@ -69,7 +68,7 @@ class UTILS:
         color_msg.color.g = self._normalize(g)
         color_msg.color.b = self._normalize(b)
         return color_msg
-        
+
     def set_leds(self, hex, brightness=100, freq=0.0):
         led_msg = LEDControl()
         data = [self._color(hex, brightness, freq)]
