7319a4da56a610000de42873eb35c21bc1c305e5
Rensdebee <r.35rens@gmail.com>
2024-11-04 20:23:35 +0100

    Refactor code

--- src/lab1/lab1/line_follower.py
+++ src/lab1/lab1/line_follower.py
@@ -12,6 +12,10 @@ from lab1.utils import UTILS
 
 class LineFollower(Node):
     def __init__(self):
+        # Set instructions
+        self.should_move = False
+        self.display_gray = False
+
         # Init node
         super().__init__("line_follower")
 
@@ -29,11 +33,14 @@ class LineFollower(Node):
         self.timer = self.create_timer(
             timer_period_sec=0.06, callback=self.timer_callback
         )
-        self.should_move = False
-        self.finalization_countdown = None
+
+        self.publisher = self.create_publisher(
+            Twist, "/cmd_vel", rclpy.qos.qos_profile_system_default
+        )
 
         self.utils.set_leds("#f2c40c", brightness=10)
         self.utils.draw_text("Setting up..")
+        self.frame = 0
 
     def crop_size(self, height, width):
         """
@@ -42,7 +49,23 @@ class LineFollower(Node):
         (Height_upper_boundary, Height_lower_boundary,
         Width_left_boundary, Width_right_boundary)
         """
-        return (0, height, 0, width)
+        # Set region to crop (Only save start as this is need to calculate offset)
+        self.crop_h_start, crop_h_stop, self.crop_w_start, crop_w_stop = (
+            0,
+            height,
+            180,
+            360,
+        )
+        # Define region center
+        self.crop_h_center = self.crop_h_start + (crop_h_stop - self.crop_h_start) / 2
+        self.crop_w_center = self.crop_w_start + (crop_w_stop - self.crop_w_start) / 2
+        assert self.crop_w_center >= 0
+        assert self.crop_h_center >= 0
+        # Define point to focus on for error
+        self.w_focus = self.crop_w_center
+        self.h_focus = self.crop_h_center - 100
+
+        return self.crop_h_start, crop_h_stop, self.crop_w_start, crop_w_stop
 
     def get_lines(self, mask, out):
         """
@@ -57,9 +80,9 @@ class LineFollower(Node):
             mask,
             rho=1,
             theta=np.pi / 180,
-            threshold=75,
-            minLineLength=40,
-            maxLineGap=1,
+            threshold=300,
+            minLineLength=80,
+            maxLineGap=150,
         )
 
         # Draw lines on original image
@@ -75,29 +98,68 @@ class LineFollower(Node):
     def image_callback(self, data):
         # Convert ROS Image message to OpenCV image
         current_frame = self.br.imgmsg_to_cv2(data)
-        # TODO Image corretion code here
-        self.image = current_frame
+        # TODO Read from file
+        mtx = np.array(
+            [
+                [311.717726, 0.000000, 307.396186],
+                [0.000000, 313.319557, 191.237882],
+                [0.000000, 0.000000, 1.000000],
+            ]
+        )
+        dist = np.array([-0.244207, 0.043980, 0.000955, -0.000878, 0.000000])
+        h = current_frame.shape[0]
+        w = current_frame.shape[1]
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 0, (w, h))
+        self.image = cv2.undistort(current_frame, mtx, dist, None, newcameramtx)
 
     def edge_detector(self, current_frame):
-        # TODO Maybe import this from edge_detector.py with a command line argument to do different methods
-
         # Convert the image to grayscale
         current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
 
         # Apply a threshold to binarize the image (only keep bright areas)
-        _, binary = cv2.threshold(current_frame, 250, 255, cv2.THRESH_BINARY)
+        _, binary = cv2.threshold(current_frame, 200, 255, cv2.THRESH_BINARY)
 
+        # TODO Maybe import this from edge_detector.py with a command line argument to do different methods
         # Setting parameter values
         t_lower = 50  # Lower Threshold
         t_upper = 150  # Upper threshold
-
         # Applying the Canny Edge filter
         canny = cv2.Canny(binary, t_lower, t_upper)
 
         # Dilate the edges to make it easier for Hough transform
-        kernel = np.ones((7, 7), np.uint8)
+        kernel = np.ones((10, 10), np.uint8)
         edges_dilated = cv2.dilate(canny, kernel)
         return edges_dilated, binary
+
+    def get_closest_line(
+        self,
+        lines,
+    ):
+        # Get closest line
+        abs_error = np.inf
+        rot_error = None
+        min_i = None
+
+        for i in range(0, len(lines)):
+            line = lines[i][0]
+            line_center_w = self.crop_w_start + ((line[0] + line[2]) / 2)
+            line_center_h = self.crop_h_start + ((line[1] + line[3]) / 2)
+
+            line_error_w = self.w_focus - line_center_w
+            line_error_h = self.h_focus - line_center_h
+
+            abs_total_error = np.abs(line_error_w) + 2 * np.abs(line_error_h)
+
+            if abs_total_error < abs_error:
+                abs_error = abs_total_error
+                rot_error = line_error_w
+                min_i = i
+
+        # Return line with lowest error and width error
+        if min_i is not None:
+            return lines[min_i][0], rot_error
+        else:
+            return None, None
 
     def timer_callback(self):
         # Wait for the first image to be received
@@ -106,11 +168,17 @@ class LineFollower(Node):
 
         # Get image stats
         image = self.image.copy()
+        # cv2.imwrite(f"./imgs/frame_{self.frame}.png", image)
+        self.frame += 1
         self.height = image.shape[0]
         self.width = image.shape[1]
         # Edge detection
         mask, binary = self.edge_detector(image)
-        image = cv2.cvtColor(binary, cv2.COLOR_GRAY2BGR)
+
+        # Display binary image
+        if self.display_gray:
+            image = cv2.cvtColor(binary, cv2.COLOR_GRAY2BGR)
+
         # Crop only top part to detect lines from
         crop_h_start, crop_h_stop, crop_w_start, crop_w_stop = self.crop_size(
             self.height, self.width
@@ -121,33 +189,26 @@ class LineFollower(Node):
         lines = self.get_lines(
             crop, image[crop_h_start:crop_h_stop, crop_w_start:crop_w_stop]
         )
+        line, rot_error = self.get_closest_line(lines)
 
-        # Get closest line
-        error = np.inf
-        min_i = None
-        for i in range(0, len(lines)):
-            line = lines[i][0]
-            line_center_w = crop_w_start + ((line[0] + line[2]) / 2)
-            line_error = line_center_w - (self.width / 2)
-            cv2.circle(
-                image,
-                (int(line_center_w), 0),
-                5,
-                (255, 255, 0),
-                7,
-            )
-            cv2.circle(
-                image,
-                (int(self.width / 2), 0),
-                5,
-                (0, 255, 255),
-                7,
-            )
-            if np.abs(line_error) < error:
-                error = np.abs(line_error)
-                min_i = i
-        if min_i is not None:
-            line = lines[min_i][0]
+        if line is not None:
+            self.utils.set_leds("#0000FF")
+            # < 0  IS RIGHT > 0 IS LEFT
+            turn = rot_error * 0.5
+
+            if turn < 0:
+                self.utils.draw_text("Going Right")
+                print("Right")
+            else:
+                self.utils.draw_text("Going Left")
+                print("Left")
+
+            if self.should_move:
+                message = Twist()
+                message.angular.z = turn
+                message.linear.x = 0.5
+                self.publisher.publish(message)
+
             # Draw middle point of closest line
             cv2.circle(
                 image,
@@ -159,6 +220,14 @@ class LineFollower(Node):
                 (0, 255, 0),
                 7,
             )
+        else:
+            self.utils.set_leds("#FF0000")
+            # Draw number of lines detected to RAE screen
+            self.utils.draw_text(f"{len(lines)} lines")
+            if self.should_move:
+                message.angular.z = 0.5
+            empty_message = Twist()
+            self.publisher.publish(empty_message)
 
         # Draw box which has been cropped
         cv2.rectangle(
@@ -169,15 +238,20 @@ class LineFollower(Node):
             2,
         )
 
-        # Draw number of lines detected to RAE screen
-        self.utils.draw_text(f"{len(lines)} lines")
+        # Draw point used for error calculation
+        cv2.circle(
+            image,
+            (int(self.w_focus), int(self.h_focus)),
+            5,
+            (0, 255, 255),
+            7,
+        )
 
         # Show the output image to the user
         cv2.imshow("output", image)
         # Print the image for 5milis, then resume execution
         cv2.waitKey(5)
 
-        # TODO Turn robot towards center of closest line and drive forward
         # TODO Define stop condition
 
 
